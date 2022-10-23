8828279e1b0842e8ef7ba512c493355c7644349e
Ravil <lllx2@yandex.ru>
2022-10-23 14:20:52 +0300

    2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -5,38 +5,14 @@ Console.WriteLine($"Задайте количество столбцов (n)");
 int n = Convert.ToInt32(Console.ReadLine());
 int [,] nums = new int [m, n];
 FillArr(nums);
-PrintArr(nums);
-SumNums(nums);
-
-void PrintArr(int [,] num) {
-    for(int i =0; i<num.GetLength(0); i++) {
-        for(int j =0; j<num.GetLength(1); j++) {
-            Console.Write($"{num [i, j]} ");
-        }
-        Console.WriteLine();
-    }
-
-}
 
 void FillArr (int [,] num) {
     for(int i =0; i<num.GetLength(0); i++) {
         for(int j =0; j<num.GetLength(1); j++) {
-            num [i, j] = new Random().Next(1,10);
+            num [i, j] = i+j;
+            Console.Write(num[i,j] + " ");
 
         }
+        Console.WriteLine();
     }
-}
-
-void SumNums(int [,] num) {
-    int sum = 0;
-    for(int i =0; i<num.GetLength(0); i++) {
-        for(int j =0; j<num.GetLength(1); j++) {
-            if(i==j) {
-                sum += num[i,j];
-            }
-            
-        }
-    }
-    Console.WriteLine();
-    Console.WriteLine(sum);
 }
